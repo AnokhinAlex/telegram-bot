81aba2589e0c3dbe81814a6633856f6c9f09ac51
AnokhinAlex <anokhinalexeyolegovych@gmail.com>
2017-07-26 14:17:09 +0300

    second_a

--- Mytst5568bot.php
+++ Mytst5568bot.php
@@ -4,15 +4,15 @@ $link = mysqli_connect(DB_HOST,DB_LOGIN,DB_PASSWORD,DB_NAME);
 echo mysqli_error($link);
 $access_token = '393757007:AAEg2nokJasgly-ku_lY3vM50IhR8ZKKXpI';
 $api = 'https://api.telegram.org/bot' . $access_token;
-$output = json_decode(file_get_contents('php://input'), TRUE); // Получим то, что передано скрипту ботом в POST-сообщении и распарсим
-$chat_id = $output['message']['chat']['id']; // Выделим идентификатор чата
-$first_name = $output['message']['chat']['first_name']; // Выделим имя собеседника
-$message = $output['message']['text']; // Выделим сообщение собеседника ыяыва
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$first_name = $output['message']['chat']['first_name'];
+$message = $output['message']['text'];
 
+$tolower = mb_strtolower($message, 'UTF-8');
 $str = substr($message,0,21); 
 $str_canonical = "View all finish user:10";
 if((substr($str_canonical,0,21) == substr($message,0,21)) ){
-    // sendMessage($chat_id, 'прошла проверка' );
     $st = $message; 
     list($number) = sscanf($st, "View all finish user:%d");
         if(is_int($number) && ($number > 0) ){
@@ -25,8 +25,8 @@ if((substr($str_canonical,0,21) == substr($message,0,21)) ){
             }
         }
 } else {
-    
-switch(strtolower_ru($message)) {
+     
+switch( $tolower) {
 
 case ('xnode-хочу работать!'):
     
@@ -63,18 +63,4 @@ break;
 
 function sendMessage($chat_id, $message) {
 file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
-}
-
-/**
-* Функция перевода символов в нижний регистр, учитывающая кириллицу
-*/
-
-function strtolower_ru($text) {
-
-   $alfavitlover = array('ё','й','ц','у','к','е','н','г', 'ш','щ','з','х','ъ','ф','ы','в', 'а','п','р','о','л','д','ж','э', 'я','ч','с','м','и','т','ь','б','ю');
-
-     $alfavitupper = array('Ё','Й','Ц','У','К','Е','Н','Г', 'Ш','Щ','З','Х','Ъ','Ф','Ы','В', 'А','П','Р','О','Л','Д','Ж','Э', 'Я','Ч','С','М','И','Т','Ь','Б','Ю');
-
-return str_replace($alfavitupper,$alfavitlover,strtolower($text));
-
 }
